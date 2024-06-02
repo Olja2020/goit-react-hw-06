dbd621c7a788b1a52583f231c274937626c4234e
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-06-02 17:38:24 +0300

    add

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,24 +17,14 @@ const persistedContactsReducer = persistReducer(
   {
     key: "selectedContact",
     storage,
-    whitelist: ["contact"],
+    whitelist: ["items"],
   },
   contactsReducer
 );
-
-const persistedfiltersReducer = persistReducer(
-  {
-    key: "selectedName",
-    storage,
-    whitelist: ["name"],
-  },
-  filtersReducer
-);
-
 export const store = configureStore({
   reducer: {
     contacts: persistedContactsReducer,
-    filters: persistedfiltersReducer,
+    filters: filtersReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
@@ -45,29 +35,4 @@ export const store = configureStore({
 });
 
 export const persistor = persistStore(store);
-// import { combineReducers, configureStore } from "@reduxjs/toolkit";
-// import { contactReducer } from "./contactsSlice";
-// import { filterReducer } from "./filtersSlice";
-// import persistReducer from "redux-persist/es/persistReducer";
-// import persistStore from "redux-persist/es/persistStore";
-// import storage from "redux-persist/lib/storage";
 
-// const persistConfig = {
-//   key: "contacts",
-//   storage,
-// };
-
-// const rootReducer = combineReducers({
-//   contacts: persistReducer(persistConfig, contactReducer),
-//   filters: filterReducer,
-// });
-
-// export const store = configureStore({
-//   reducer: rootReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false,
-//     }),
-// });
-
-// export const persistor = persistStore(store);
